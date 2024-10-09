80227064119b94fe48705a6c3d168ed1b8000774
BlvckCatt <mueresnowy5456@gmail.com>
2024-10-09 18:57:05 -0300

    ok

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private navCtrl: NavController) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  login() {
+    if (this.loginForm.valid) {
+      // Procesar el inicio de sesión
+      console.log('Sesión iniciada');
+      this.navCtrl.navigateRoot('/home'); // Redirigir a la página principal
+    }
+  }
 }
