bf6674c05651d04b45c02e74dc70b34de591656b
BlvckCatt <mueresnowy5456@gmail.com>
2024-10-15 18:37:42 -0300

    ok

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -8,31 +8,31 @@ import { RegistrologinService } from 'src/app/services/registrologin.service';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage {
-  username: string | undefined; 
-  email: string | undefined; 
+  username: string | undefined;
+  email: string | undefined;
+  isLoggedIn: boolean = false; // Verifica si el usuario está logueado
 
   constructor(private router: Router, private registrologinService: RegistrologinService) {
-    const user = this.registrologinService.getCurrentUser();
+    const user = this.registrologinService.getCurrentUser(); // Obtiene el usuario actual
     if (user) {
-      this.username = user.username; 
-      this.email = user.email; 
+      this.username = user.username; // Asigna el nombre de usuario
+      this.email = user.email; // Asigna el correo electrónico
+      this.isLoggedIn = true; // Indica que el usuario está logueado
     }
   }
 
   goToSettings() {
-    this.router.navigate(['/configuracion']); 
+    this.router.navigate(['/configuracion']); // Navega a la página de configuración
   }
 
   logOut() {
+    // Lógica para cerrar sesión
+    this.registrologinService.logOut(); // Desconecta al usuario en el servicio
     console.log('Sesión cerrada');
-    this.router.navigate(['/login']); 
+    this.router.navigate(['/login']); // Redirige a la página de inicio de sesión
   }
 
   goToLogin() {
-    this.router.navigate(['/login']); 
-  }
-
-  goTolistaDonas(){
-    this.router.navigate(['/lista-donas']); 
+    this.router.navigate(['/login']); // Redirigir al login
   }
 }
